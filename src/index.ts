1d28670048ce88390ed4b2418cb25a03e648142b
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2020-04-28 17:14:40 +0900

    Add function to compare object.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export enum ObjectType {
+  Array,
   Bigint,
   Boolean,
   Class,
@@ -11,13 +12,22 @@ export enum ObjectType {
   Undefined,
 }
 
+export type Equal = { equal: true }
+
+export type NotEqual = { equal: false }
+
+export type Result = Equal | NotEqual
+
+const EQUAL: Equal = { equal: true }
+const NOT_EQUAL: NotEqual = { equal: false }
+
 export const typeOf = (obj: any): ObjectType => {
-  switch(typeof obj) {
-    case 'bigint': 
+  switch (typeof obj) {
+    case 'bigint':
       return ObjectType.Bigint
-    case 'boolean': 
+    case 'boolean':
       return ObjectType.Boolean
-    case 'function': 
+    case 'function':
       return ObjectType.Function
     case 'number':
       return ObjectType.Number
@@ -28,13 +38,69 @@ export const typeOf = (obj: any): ObjectType => {
     case 'undefined':
       return ObjectType.Undefined
   }
-  
+
   if (obj === null) {
     return ObjectType.Null
+  }
+  if (Array.isArray(obj)) {
+    return ObjectType.Array
   }
   if (obj.constructor.name !== 'Object') {
     return ObjectType.Class
   }
-  
+
   return ObjectType.Object
+}
+
+export const compareArray = (arr1: any[], arr2: any[]): Result => {
+  if (arr1.length !== arr2.length) {
+    return NOT_EQUAL
+  }
+
+  const dupArr1 = [...arr1].sort(), dupArr2 = [...arr2].sort()
+
+  return [...new Array(arr1.length).keys()]
+    .map(idx => [dupArr1[idx], dupArr2[idx]])
+    .every(([obj1, obj2]) => compareObject(obj1, obj2))
+      ? EQUAL
+      : NOT_EQUAL
+}
+
+export const compareObject = (obj1: any, obj2: any): Result => {
+  const obj1Type = typeOf(obj1)
+  const obj2Type = typeOf(obj2)
+
+  if (obj1Type != obj2Type) {
+    return NOT_EQUAL
+  }
+
+  switch (obj1Type) {
+    case ObjectType.Bigint:
+    case ObjectType.Boolean:
+    case ObjectType.Number:
+    case ObjectType.String:
+    case ObjectType.Symbol:
+      return obj1 === obj2 ? EQUAL : NOT_EQUAL
+    case ObjectType.Null:
+    case ObjectType.Undefined:
+      return EQUAL
+    case ObjectType.Function:
+      return obj1.name === obj2.name ? EQUAL : NOT_EQUAL
+    case ObjectType.Class:
+      return obj1.constructor.name === obj2.constructor.name ? EQUAL : NOT_EQUAL
+    case ObjectType.Array:
+      return compareArray(obj1, obj2)
+  }
+
+  const obj1Keys = Object.keys(obj1)
+  if (obj1Keys.length !== Object.keys(obj2).length) {
+    return NOT_EQUAL
+  }
+  if (obj1Keys.length === 0) {
+    return EQUAL
+  }
+
+  return obj1Keys.every(key => compareObject(obj1[key], obj2[key]).equal)
+    ? EQUAL
+    : NOT_EQUAL
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,14 @@
 import {
   ObjectType,
   typeOf,
+  compareObject
 } from '~/index'
 
 describe('typeOf', () => {
+  test('Verification of a array type.', () => {
+    expect(typeOf([])).toEqual(ObjectType.Array)
+  })
+
   test('Verification of a bigint type.', () => {
     expect(typeOf(BigInt(0))).toEqual(ObjectType.Bigint)
   })
@@ -43,5 +48,140 @@ describe('typeOf', () => {
 
   test('Verificationn of a undefined.', () => {
     expect(typeOf(undefined)).toEqual(ObjectType.Undefined)
+  })
+})
+
+describe('compareObject', () => {
+  describe('When different object type.', () => {
+    test('Return value equals false', () => {
+      expect(compareObject({ key: 'value' }, 'foo bar baz')).toEqual({ equal: false })
+    })
+  })
+
+  describe('when same object type.', () => {
+    describe('When the Array type.', () => {
+      test('Return type equal true if blank array.', () => {
+        expect(compareObject([], [])).toEqual({ equal: true })
+      })
+
+      test('Return type equal false if arrays of dirrefent length.', () => {
+        expect(compareObject([], [{ foo: 'bar' }])).toEqual({ equal: false })
+      })
+
+      test('Return type equal true of same array.', () => {
+        expect(compareObject([{ foo: 'bar' }, { baz: 'qux' }, 1], [1, { foo: 'bar' }, { baz: 'qux'}])).toEqual({ equal: true })
+      })
+    })
+
+    describe('When the BigInt type.', () => {
+      test('Return type equal true.', () => {
+        expect(compareObject(BigInt(1), BigInt(1))).toEqual({ equal: true })
+      })
+
+      test('Return type equal false.', () => {
+        expect(compareObject(BigInt(1), BigInt(2))).toEqual({ equal: false })
+      })
+    })
+
+    describe('When the Boolean type.', () => {
+      test('Return type equal true.', () => {
+        expect(compareObject(false, false)).toEqual({ equal: true })
+      })
+
+      test('Return type equal false.', () => {
+        expect(compareObject(true, false)).toEqual({ equal: false })
+      }) 
+    })
+
+    describe('When then Class type.', () => {
+      class Foo {}
+      class Bar {}
+
+      test('Return type equal true.', () => {
+        expect(compareObject(new Foo(), new Foo())).toEqual({ equal: true })
+      })
+
+      test('Return type equal false.', () => {
+        expect(compareObject(new Foo(), new Bar())).toEqual({ equal: false })
+      })
+    })
+
+    describe('When the Function type.', () => {
+      const fn1 = () => {}
+      
+      test('Return type equal true.', () => {
+        expect(compareObject(fn1, fn1)).toEqual({ equal: true })
+      })
+
+      test('Return type equal false.', () => {
+        expect(compareObject(fn1, () => {})).toEqual({ equal: false })
+      })
+    })
+
+    describe('when then Number type.', () => {
+      test('Return type equal true.', () => {
+        expect(compareObject(1, 1)).toEqual({ equal: true})
+      })
+
+      test('Return type equal false.', () => {
+        expect(compareObject(1, 2)).toEqual({ equal: false })
+      })
+    })
+
+    describe('When the Null type.', () => {
+      test('Return type equal true.', () => {
+        expect(compareObject(null, null)).toEqual({ equal: true })
+      })
+    })
+
+    describe('When the Object type.', () => {
+      test('Return type equal true if blank object.', () => {
+        expect(compareObject({}, {})).toEqual({ equal: true })
+      })
+
+      test('Return type Equal true if same objects.', () => {
+        expect(compareObject({ foo: 'bar', baz: 'qux', key: 1 }, { baz: 'qux', key: 1, foo: 'bar' })).toEqual({ equal: true })
+      })
+
+      test('Return type equal false if objects of dirrefent key size.', () => {
+        expect(compareObject({}, { foo: 'bar' })).toEqual({ equal: false })
+      })
+
+      test('Return type equal false if objects of different key.', () => {
+        expect(compareObject({ foo: 'baz' }, { bar: 'baz' })).toEqual({ equal: false })
+      })
+
+      test('Return type equal false if objects of different value.', () => {
+        expect(compareObject({ foo: 'bar' }, { foo: 'baz' })).toEqual({ equal: false })
+      })
+    })
+
+    describe('When the String type', () => {
+      test('Return type equal true.', () => {
+        expect(compareObject('foo', 'foo')).toEqual({ equal: true })
+      })
+
+      test('Return type equal false.', () => {
+        expect(compareObject('foo', 'bar')).toEqual({ equal: false })
+      })
+    })
+
+    describe('When the Symbol type.', () => {
+      const obj1 = Symbol('obj1')
+      
+      test('Return type equal true', () => {
+        expect(compareObject(obj1, obj1)).toEqual({ equal: true })
+      })
+
+      test('Return type equal false', () => {
+        expect(compareObject(obj1, Symbol('obj1'))).toEqual({ equal: false })
+      })
+    })
+
+    describe('When the Undefined type.', () => {
+      test('Return type equals true.', () => {
+        expect(compareObject(undefined, undefined)).toEqual({ equal: true })
+      })
+    })
   })
 })
